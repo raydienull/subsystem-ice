aa648539026102cff1c39f6708ce9e60b85366bd
Raydie <50953519+raydienull@users.noreply.github.com>
2025-11-05 12:45:30 +0100

    f

--- Source/OnlineSubsystemICE/Public/OnlineSubsystemICEPackage.h
+++ Source/OnlineSubsystemICE/Public/OnlineSubsystemICEPackage.h
@@ -4,10 +4,6 @@
 
 #include "CoreMinimal.h"
 
-/** Pre-pend to log messages */
-#undef ONLINE_LOG_PREFIX
-#define ONLINE_LOG_PREFIX TEXT("ICE: ")
-
 /** Logging for OnlineSubsystemICE */
 DECLARE_LOG_CATEGORY_EXTERN(LogOnlineICE, Log, All);
 
